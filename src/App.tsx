cfa05b3244a8db9462313c6c51290e6ed1f97f81
Jackson Ming Hu <huming2207@gmail.com>
2020-07-04 12:14:33 +1000

    Apply a fresher theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,47 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Login from "./Login";
+import Register from "./Register";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#68b36b",
+      main: "#43a047",
+      dark: "#2e7031",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ffac33",
+      main: "#ff9800",
+      dark: "#b26a00",
+      contrastText: "#000",
+    },
+  },
+});
 
 function App(): JSX.Element {
   return (
     <div className="App">
-      <Switch>
-        <Route path="/about"></Route>
+      <ThemeProvider theme={theme}>
+        <Switch>
+          <Route path="/about"></Route>
+          <Route path="/register">
+            <Register />
+          </Route>
 
-        {/* Note how these two routes are ordered. The more specific
-            path="/contact/:id" comes before path="/contact" so that
-            route will render when viewing an individual contact */}
-        <Route path="/contact/:id"></Route>
-        <Route path="/contact"></Route>
-
-        {/* If none of the previous routes render anything,
+          {/* If none of the previous routes render anything,
             this route acts as a fallback.
 
             Important: A route with path="/" will *always* match
             the URL because all URLs begin with a /. So that's
             why we put this one last of all */}
-        <Route path="/">
-          <Login />
-        </Route>
-      </Switch>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </ThemeProvider>
     </div>
   );
 }
