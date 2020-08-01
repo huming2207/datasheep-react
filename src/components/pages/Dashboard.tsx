875d749507558ec85d29cd02b37a482f976786df
Jackson Ming Hu <huming2207@gmail.com>
2020-08-01 14:17:30 +1000

    dashboard: add in kanbanlist

--- src/components/pages/Dashboard.tsx
+++ src/components/pages/Dashboard.tsx
@@ -2,10 +2,16 @@ import React, { useState } from "react";
 import Footer from "../common/Footer";
 import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { KanbanList } from "../common/KanbanList";
 
 export function Dashboard(): JSX.Element {
   return (
     <Container maxWidth="xs">
+      <DndProvider backend={HTML5Backend}>
+        <KanbanList></KanbanList>
+      </DndProvider>
       <Box mt={5}>
         <Footer />
       </Box>
