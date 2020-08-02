eae728c08cf5d0346fcf2292b322b0d328649cf6
Jackson Ming Hu <huming2207@gmail.com>
2020-08-02 11:49:45 +1000

    Switch to react-trello

--- src/components/pages/Dashboard.tsx
+++ src/components/pages/Dashboard.tsx
@@ -2,16 +2,11 @@ import React from "react";
 import Footer from "../common/Footer";
 import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import { KanbanList } from "../common/KanbanList";
+import { Grid } from "@material-ui/core";
 
 export function Dashboard(): JSX.Element {
   return (
-    <Container maxWidth="xs">
-      <DndProvider backend={HTML5Backend}>
-        <KanbanList></KanbanList>
-      </DndProvider>
+    <Container maxWidth="md">
       <Box mt={5}>
         <Footer />
       </Box>
