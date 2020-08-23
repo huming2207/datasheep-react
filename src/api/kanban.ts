f728ab4aac1c22b8a3deef8652d72f7872dfae23
Jackson Ming Hu <huming2207@gmail.com>
2020-08-23 22:39:51 +1000

    api: Implement API client for kanban

--- src/api/kanban.ts
+++ src/api/kanban.ts
@@ -1,39 +1,75 @@
-import { CommonProps, LaneProps, CardProps } from "react-trello";
+import axios from "axios";
+import { CommonProps, LaneProps, CardProps, BoardData } from "react-trello";
+import { loginWithJwt } from "./auth";
+import { RelatedProjectReply } from "./types/Project";
 
-export const processCardDrag = (
+export interface CardDragInput {
+  srcList: string;
+  dstList: string;
+  idx: number;
+}
+
+export const fetchList = async (projectId: string): Promise<BoardData> => {
+  const result = await axios.get<RelatedProjectReply>(`/project/${projectId}/list`);
+  const lists = result.data.data.lists;
+  if (lists.length < 1) return { lanes: [] };
+
+  const lanes: LaneProps[] = [];
+  for (const list of lists) {
+    const lane: LaneProps = { title: list.title, id: list._id, label: list.description };
+    if (list.events) {
+      for (const event of list.events) {
+        const card: CardProps = { title: event.title, description: event.content, id: event._id };
+        if (!lane.cards) lane.cards = [];
+        lane.cards.push(card);
+      }
+    }
+
+    lanes.push(lane);
+  }
+
+  return { lanes };
+};
+
+export const handleCardDrag = async (
   cardId: string,
   srcLaneId: string,
   targetLaneId: string,
   idx: number,
   card: CommonProps | unknown,
-): boolean => {
-  console.log(cardId);
-  console.log(srcLaneId);
-  console.log(targetLaneId);
-  console.log(idx);
-  console.log(card);
+): Promise<boolean> => {
+  console.log(cardId, srcLaneId, targetLaneId, idx, card);
+
+  const input: CardDragInput = { srcList: srcLaneId, dstList: targetLaneId, idx };
+  try {
+    const result = await axios.put(`/event/${cardId}/move`, input, loginWithJwt());
+    console.log(result);
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+
   return true;
 };
 
-export const processLaneDrag = (removedIdx: number, addedIdx: number, lane: LaneProps): void => {
+export const handleLaneDrag = async (removedIdx: number, addedIdx: number, lane: LaneProps): Promise<void> => {
   console.log(removedIdx);
   console.log(addedIdx);
   console.log(lane);
 };
 
-export const processCardAdd = (card: CardProps, laneId: string): void => {
-  console.log(laneId);
-  console.log(card);
+export const handleCardAdd = async (card: CardProps, laneId: string): Promise<void> => {
+  console.log(card, laneId);
 };
 
-export const processCardDelete = (cardId: string, laneId: string): void => {
+export const handleCardDelete = async (cardId: string, laneId: string): Promise<void> => {
   console.log(cardId, laneId);
 };
 
-export const processLaneAdd = (lane: LaneProps): void => {
+export const handleLaneAdd = async (lane: LaneProps): Promise<void> => {
   console.log(lane);
 };
 
-export const processLaneDelete = (laneId: string): void => {
+export const handleLaneDelete = async (laneId: string): Promise<void> => {
   console.log(laneId);
 };
