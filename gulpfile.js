d6b9621bd7afdd88517232387f4d5776b0fa0085
Jackson Ming Hu <huming2207@gmail.com>
2020-09-13 10:51:27 +1000

    gulp: generate compressed bundle

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,24 @@
-const gulp = require('gulp')
-const inlinesource = require('gulp-inline-source')
-const replace = require('gulp-replace')
+/* eslint-disable @typescript-eslint/no-var-requires */
+const gulp = require("gulp");
+const inlinesource = require("gulp-inline-source");
+const replace = require("gulp-replace");
+const gzip = require("gulp-gzip");
 
-gulp.task('default', () => {
-    return gulp.src('./build/*.html')
-        .pipe(replace('.js"></script>', '.js" inline></script>'))
-        .pipe(replace('rel="stylesheet">', 'rel="stylesheet" inline>'))
-        .pipe(inlinesource({
-            compress: false,
-            ignore: ['png']
-        }))
-        .pipe(gulp.dest('./build'))
+gulp.task("default", () => {
+  return gulp
+    .src("./build/*.html")
+    .pipe(replace('.js"></script>', '.js" inline></script>'))
+    .pipe(replace('rel="stylesheet">', 'rel="stylesheet" inline>'))
+    .pipe(
+      inlinesource({
+        compress: false,
+        ignore: ["png"],
+      }),
+    )
+    .pipe(
+      gzip({
+        gzipOptions: { level: 9 },
+      }),
+    )
+    .pipe(gulp.dest("./build/bundled"));
 });
